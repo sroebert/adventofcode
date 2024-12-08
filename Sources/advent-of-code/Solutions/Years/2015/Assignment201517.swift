91cf8d16be86cfdcac8d58bd2f1f0ac57220dbdb
Steven Roebert <steven@roebert.nl>
2024-12-08 01:11:24 +0100

    Added 2015-17

--- Sources/advent-of-code/Solutions/Years/2015/Assignment201517.swift
+++ Sources/advent-of-code/Solutions/Years/2015/Assignment201517.swift
@@ -3,10 +3,57 @@ struct Assignment201517: Assignment {
     // MARK: - Assignment
     
     func solvePart1() async throws -> AssignmentOutput {
-        return ""
+        let buckets = try await getBuckets()
+        return determineBucketCounts(forFillingBuckets: buckets, withEggNogLiters: 150).count
     }
     
     func solvePart2() async throws -> AssignmentOutput {
-        return ""
+        let buckets = try await getBuckets()
+        let bucketCounts = determineBucketCounts(forFillingBuckets: buckets, withEggNogLiters: 150)
+        guard let min = bucketCounts.min() else {
+            return 0
+        }
+        
+        return bucketCounts.count { $0 == min }
+    }
+    
+    var isSlowInDebug: Bool {
+        return true
+    }
+    
+    // MARK: - Utils
+    
+    private func getBuckets() async throws -> [Int] {
+        return try await mapInput {
+            guard let bucket = Int($0) else {
+                throw InputError(message: "Invalid input")
+            }
+            return bucket
+        }
+    }
+    
+    private func determineBucketCounts(
+        forFillingBuckets allBuckets: [Int],
+        withEggNogLiters expectedLiters: Int
+    ) -> [Int] {
+        var stack = [(liters: 0, bucketCount: 0, buckets: ArraySlice(allBuckets))]
+        
+        var bucketCounts: [Int] = []
+        while !stack.isEmpty {
+            let (liters, bucketCount, buckets) = stack.removeLast()
+            
+            guard liters < expectedLiters else {
+                if liters == expectedLiters {
+                    bucketCounts.append(bucketCount)
+                }
+                continue
+            }
+            
+            buckets.enumerated().forEach { index, bucket in
+                stack.append((liters + bucket, bucketCount + 1, buckets.suffix(from: buckets.startIndex + index + 1)))
+            }
+        }
+        
+        return bucketCounts
     }
 }
