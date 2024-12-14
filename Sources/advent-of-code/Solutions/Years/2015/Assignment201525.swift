c25a95a44cba6310fb939c369fe8af2da9e946b8
Steven Roebert <steven@roebert.nl>
2024-12-14 19:54:32 +0100

    Added 2015-25

--- Sources/advent-of-code/Solutions/Years/2015/Assignment201525.swift
+++ Sources/advent-of-code/Solutions/Years/2015/Assignment201525.swift
@@ -3,10 +3,47 @@ struct Assignment201525: Assignment {
     // MARK: - Assignment
     
     func solvePart1() async throws -> AssignmentOutput {
-        return ""
+        let (codeRow, codeColumn) = try await getRowColumn()
+        
+        var value = 20151125
+        
+        var startRow = 2
+        var row = startRow
+        var column = 1
+        
+        while true {
+            value = (value * 252533) % 33554393
+            
+            if row == codeRow && column == codeColumn {
+                return value
+            }
+            
+            row -= 1
+            column += 1
+            
+            if row == 0 {
+                startRow += 1
+                row = startRow
+                column = 1
+            }
+        }
     }
     
     func solvePart2() async throws -> AssignmentOutput {
-        return ""
+        return "N/A"
+    }
+    
+    // MARK: - Utils
+    
+    private func getRowColumn() async throws -> (row: Int, column: Int) {
+        let input = try await getInput()
+        guard let match = input.firstMatch(of: /row (?<row>\d+), column (?<column>\d+)\./) else {
+            throw InputError(message: "Invalid input")
+        }
+        
+        return (
+            row: Int(match.output.row) ?? 0,
+            column: Int(match.output.column) ?? 0
+        )
     }
 }
