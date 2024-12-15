e791ac5354ce5fb8465e987f2b092bb56f06176f
Steven Roebert <steven@roebert.nl>
2024-12-15 20:41:34 +0100

    Added 2016-3

--- Sources/advent-of-code/Solutions/Years/2016/Assignment201603.swift
+++ Sources/advent-of-code/Solutions/Years/2016/Assignment201603.swift
@@ -3,10 +3,61 @@ struct Assignment201603: Assignment {
     // MARK: - Assignment
     
     func solvePart1() async throws -> AssignmentOutput {
-        return ""
+        let triangles = try await getTriangles()
+        return triangles.count(where: \.isValid)
     }
     
     func solvePart2() async throws -> AssignmentOutput {
-        return ""
+        let triangles = try await getTriangles()
+        let realTriangles = stride(from: 0, to: triangles.count, by: 3).flatMap { index in
+            [
+                Triangle(
+                    side1: triangles[index].side1,
+                    side2: triangles[index+1].side1,
+                    side3: triangles[index+2].side1
+                ),
+                Triangle(
+                    side1: triangles[index].side2,
+                    side2: triangles[index+1].side2,
+                    side3: triangles[index+2].side2
+                ),
+                Triangle(
+                    side1: triangles[index].side3,
+                    side2: triangles[index+1].side3,
+                    side3: triangles[index+2].side3
+                ),
+            ]
+        }
+        return realTriangles.count(where: \.isValid)
+    }
+    
+    // MARK: - Utils
+    
+    private struct Triangle {
+        var side1: Int
+        var side2: Int
+        var side3: Int
+        
+        var isValid: Bool {
+            return (
+                side1 + side2 > side3 &&
+                side2 + side3 > side1 &&
+                side1 + side3 > side2
+            )
+        }
+    }
+    
+    private func getTriangles() async throws -> [Triangle] {
+        try await mapInput { line in
+            let numbers = line.split(whereSeparator: \.isWhitespace).compactMap { Int($0) }
+            guard numbers.count == 3 else {
+                throw InputError(message: "Invalid input")
+            }
+            return Triangle(
+                side1: numbers[0],
+                side2: numbers[1],
+                side3: numbers[2]
+            )
+        }
     }
 }
